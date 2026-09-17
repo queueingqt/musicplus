4b1b0d2d3566c8a1531316b577947ee182a57fcc
lexxrexx <67492954+lexxrexx@users.noreply.github.com>
2026-09-17 15:56:50 -0700

    Fix missing Row import in ArtistDetailScreen.kt (auto-merge gap)

--- src/main/kotlin/com/lightwave/app/ArtistDetailScreen.kt
+++ src/main/kotlin/com/lightwave/app/ArtistDetailScreen.kt
@@ -1,6 +1,7 @@
 package com.lightwave.app
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.items
